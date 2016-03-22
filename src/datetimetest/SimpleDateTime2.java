ce2c7a18171f901a8eb684463c8df8d85aa9df70
trogers8 <trogers8@L11702042765.wctc.msft>
2016-03-22 16:44:36 -0500

    sfsadf

--- src/datetimetest/SimpleDateTime2.java
+++ src/datetimetest/SimpleDateTime2.java
@@ -5,23 +5,36 @@
  */
 package datetimetest;
 
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  *
  * @author trogers8
  */
 public class SimpleDateTime2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         //create simple date formatter
-        SimpleDateFormat sdf = new SimpleDateFormat();
+        String format = "M/d/yy h:mm:ss a";
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        DateFormat df = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.MEDIUM);
+        
         
         // current date and time
         Calendar date1 = Calendar.getInstance();
-        System.out.println("Current: " + date1);
+        System.out.println("Current: " + df.format(date1.getTime()));
         // add 30 days
         date1.add(Calendar.DATE, 30);
-        System.out.println("Future: " + date1);
+        System.out.println("Future: " + df.format(date1.getTime()));
+        
+        
+        //turn string into date
+        String dateString = "1/1/16 0:00:00 am";
+        Date date2 = sdf.parse(dateString);
+        sdf.format(date2);
+        System.out.println(date2);
     }
 }
